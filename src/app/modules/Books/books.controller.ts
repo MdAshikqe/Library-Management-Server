7abfa21869a7379bdfaebcd63e2f409895c99284
Ashik <asikhosen865575@gmail.com>
2025-11-25 18:48:24 +0600

    formating sendResponse

--- src/app/modules/Books/books.controller.ts
+++ src/app/modules/Books/books.controller.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from "express";
 import { BookService } from "./books.service";
+import sendResponse from "../../shared/sendResponse";
+import status from "http-status";
 
 const createBook = async (req: Request, res: Response) => {
   console.log("=======", req.body);
   const result = await BookService.createBook(req);
 
-  res.status(200).json({
-    success: false,
-    message: "Book create successfully",
+  // res.status(200).json({
+  //   success: true,
+  //   message: "Book create successfully",
+  //   data: result,
+  // });
+  sendResponse(res, {
+    statusCode: status.OK,
+    success: true,
+    message: "Create a book successfully ",
     data: result,
   });
 };
--- src/app/shared/sendResponse.ts
+++ src/app/shared/sendResponse.ts
@@ -1,1 +1,25 @@
-// const sendResponse = await;
+import { Response } from "express";
+
+const sendResponse = <T>(
+  res: Response,
+  jsonData: {
+    statusCode: number;
+    success: boolean;
+    message: string;
+    metaData?: {
+      page: number;
+      limit: number;
+      total: number;
+    };
+    data: T | null | undefined;
+  }
+) => {
+  res.status(jsonData.statusCode).json({
+    success: jsonData.success,
+    message: jsonData.message,
+    data: jsonData.data || null || undefined,
+    metaData: jsonData.metaData || null || undefined,
+  });
+};
+
+export default sendResponse;
