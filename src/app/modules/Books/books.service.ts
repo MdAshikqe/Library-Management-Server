e458fc017ea9cfc8b56b6bf14117c0e76daa4271
Ashik <asikhosen865575@gmail.com>
2025-12-04 15:56:13 +0600

    get all data with search pagination

--- src/app/modules/Books/books.service.ts
+++ src/app/modules/Books/books.service.ts
@@ -1,5 +1,9 @@
+import { Result } from "pg";
+import { Prisma } from "../../../../generated/prisma/client";
 import { prisma } from "../../../lib/prisma";
+import { PaginationHelpers } from "../../helpers/paginationHelper";
 import { IPagination } from "../../interface/pagination";
+import { bookSearchFields } from "./book.constant";
 import { IBookFilterRequest } from "./book.interface";
 
 const createBook = async (req: any) => {
@@ -10,8 +14,55 @@ const createBook = async (req: any) => {
 };
 
 const getAllBook = async (params: IBookFilterRequest, options: IPagination) => {
-  const result = await prisma.book.findMany();
-  return result;
+  const { page, limit, skip } =
+    PaginationHelpers.calculationPagination(options);
+  const { searchTerm, ...filterData } = params;
+  const andConditions: Prisma.BookWhereInput[] = [];
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: bookSearchFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereCondition: Prisma.BookWhereInput = { AND: andConditions };
+
+  const result = await prisma.book.findMany({
+    where: whereCondition,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : { createdAt: "desc" },
+  });
+  const total = await prisma.book.count({
+    where: whereCondition,
+  });
+  return {
+    metaData: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
 };
 
 export const BookServices = {
